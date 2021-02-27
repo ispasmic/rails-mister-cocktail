c1299d03b6a5de1d19f1004e35624eceb215eaac
Shu-Yi Lim <shu.lim27@gmail.com>
2021-02-27 16:50:14 +0800

    working on css

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,6 +18,12 @@ class CocktailsController < ApplicationController
     redirect_to cocktails_path
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
   end
